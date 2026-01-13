6f766e148f6bc3304843c372d051e4bf2731a00e
Justin Zhang <justinzhang20711@gmail.com>
2026-01-12 22:49:16 -0800

    feat: create mel-spectograms from loaded data

--- main.py
+++ main.py
@@ -4,16 +4,37 @@ import numpy as np
 import librosa
 import librosa.display
 import matplotlib.pyplot as plt
+import io
+import soundfile as sf
+
 
 if __name__ == "__main__":
     dataset = load_dataset("jacktol/atc-dataset")
     train_df = dataset["train"].to_pandas()
 
-    print(type(dataset["train"])) 
+    for x in train_df:
+        print(x)
+    input()
+
+    # for every sample in train_df (includes audio and text), save a plot in /data for
+    # later inspection
 
     for index, row in train_df.iterrows():
-        print(f"Row {index}: {row.keys()}") #we see the data type as 'audio' and 'text'
-        print(type(row['audio'])) 
+        audio_bytes_io = io.BytesIO(row["audio"]["bytes"])
+        y, sr = sf.read(audio_bytes_io, dtype='float32')
+
+        S = librosa.feature.melspectrogram(y=y, sr=sr, power=2.0)
+
+        S_dB = librosa.power_to_db(S, ref=np.max)
+
+        plt.figure(figsize=(10, 4))
+        librosa.display.specshow(S_dB, sr=sr, x_axis='time', y_axis='mel', cmap='coolwarm')
+        plt.colorbar(format='%+2.0f dB')
+        plt.title(f'Mel-frequency spectrogram - {row["text"]}')
+        plt.tight_layout()
+        plt.savefig(f'/data/spectrograms/{index}.png')
+        plt.close()
+        
         input()
     
     
