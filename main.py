13b70b58913916de13a9723f93c344f33c5f7802
Justin Zhang <justinzhang20711@gmail.com>
2026-01-13 16:48:08 -0800

    feat: change spectrograms to 28x28 pixels for LeNet

--- main.py
+++ main.py
@@ -2,19 +2,14 @@ from datasets import load_dataset
 import pandas as pd
 import numpy as np
 import librosa
-import librosa.display
-import matplotlib.pyplot as plt
 import io
 import soundfile as sf
+from PIL import Image
 
 
 if __name__ == "__main__":
     dataset = load_dataset("jacktol/atc-dataset")
     train_df = dataset["train"].to_pandas()
-
-    for x in train_df:
-        print(x)
-    input()
 
     # for every sample in train_df (includes audio and text), save a plot in /data for
     # later inspection
@@ -23,18 +18,15 @@ if __name__ == "__main__":
         audio_bytes_io = io.BytesIO(row["audio"]["bytes"])
         y, sr = sf.read(audio_bytes_io, dtype='float32')
 
-        S = librosa.feature.melspectrogram(y=y, sr=sr, power=2.0)
+        S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=28,   
+                                            n_fft=1024,
+                                            hop_length=len(y) // 28)
 
         S_dB = librosa.power_to_db(S, ref=np.max)
 
-        plt.figure(figsize=(10, 4))
-        librosa.display.specshow(S_dB, sr=sr, x_axis='time', y_axis='mel', cmap='coolwarm')
-        plt.colorbar(format='%+2.0f dB')
-        plt.title(f'Mel-frequency spectrogram - {row["text"]}')
-        plt.tight_layout()
-        plt.savefig(f'/data/spectrograms/{index}.png')
-        plt.close()
+        S_norm = (S_dB - S_dB.min()) / (S_dB.max() - S_dB.min())
+        S_pixels = (S_norm * 255).astype(np.uint8)
+
+        img = Image.fromarray(S_pixels)
+        img.save(f"data/spectrograms/{row['text']}.png")
         
-        input()
-    
-    
